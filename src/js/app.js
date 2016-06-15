a1d1832b345e85bbfe27987e3e0e440837a3dd38
msmichellegar <msmichellegar@gmail.com>
2016-06-15 16:10:00 +0100

    implements basic react hello world

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
-document.write('welcome to my app');
-console.log('app loadeds');
+var React = require("react");
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
